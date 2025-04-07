f9d3a0acd57eb658da376f3651451fdcec68f498
Rahul-kumar-70 <rahulkumarpatel7079@gmail.com>
2025-04-07 13:46:35 +0530

    Update settings.py

--- collegestudent/collegestudent/settings.py
+++ collegestudent/collegestudent/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5zx8gz-m223oub%!600@n+121c17*b534%jgj_$lu8&!x@*0m)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.126.67.226', '*']
 
 
 # Application definition
