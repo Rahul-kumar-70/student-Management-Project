1aac0b70097b955698041d7603dee0675b7660df
Rahul-kumar-70 <rahulkumarpatel7079@gmail.com>
2025-04-07 17:24:31 +0530

    Update settings.py

--- collegestudent/collegestudent/settings.py
+++ collegestudent/collegestudent/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=BASE_DIR/'templates'
 STATIC_DIR=BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -129,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[STATIC_DIR]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 LOGIN_REDIRECT_URL='/index'
 LOGOUT_REDIRECT_URL='/logout'
